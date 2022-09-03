740092923c260900e745b7ebcce5d4f641dd29ca
shamiul-pro <manjurulhaque552@gmail.com>
2022-09-04 01:42:25 +0600

    1 - Basic complete

---
+++ 1 - Basic/1.9 BuildingAMadLibsGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, plurarNoun, celebrity;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+            Console.Write("Enter a plurar noun: ");
+            plurarNoun = Console.ReadLine();
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(plurarNoun + " are blue");
+            Console.WriteLine("I Love " + celebrity);
+
+            Console.ReadLine(); //for pause the console
+        }
+    }
+}
