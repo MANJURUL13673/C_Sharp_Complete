a7667d7de4b21cb5002a013621677c9ddc26c3d7
shamiul-pro <manjurulhaque552@gmail.com>
2022-09-03 17:34:09 +0600

    1 - Basic

---
+++ 1 - Basic/1.3 Variables.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deferenet way to variable initialization
+            string characterName = "John";
+            int characterAge;
+            characterAge = 35;
+
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+
+            characterName = "Mike";
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+
+            Console.ReadLine(); //for pause the console
+        }
+    }
+}
