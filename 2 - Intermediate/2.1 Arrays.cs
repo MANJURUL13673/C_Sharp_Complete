4b7301f513fde90e3fd05233ecbd7082a6fd617b
shamiul-pro <manjurulhaque552@gmail.com>
2022-09-05 23:29:01 +0600

    2 - Intermediate

---
+++ 2 - Intermediate/2.1 Arrays.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumber = { 4, 8, 16, 23, 42 };
+            Console.WriteLine(luckyNumber[4]);
+            luckyNumber[3] = 2;
+            Console.WriteLine(luckyNumber[3]);
+
+            string[] friends = new string[5];
+            friends[0] = "Jim";
+            friends[1] = "Kelly";
+
+            Console.ReadLine(); //for pause the console
+        }
+    }
+}
---
+++ 2 - Intermediate/2.2 Methods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Mike", 36);
+            SayHi("John", 32);
+            SayHi("Tom", 40);
+
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you are " + age);
+        }
+    }
+}
